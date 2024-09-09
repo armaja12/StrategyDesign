f8c0c49a20243f11660668c57de404892f7a5b50
armaja12 <janinelitada@gmail.com>
2024-09-09 11:41:30 +0800

    Whole Code changes

---
+++ TollFeeContext.cs
@@ -0,0 +1,39 @@
+﻿using AssessmentConsoleApplication.Strategies;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssessmentConsoleApplication.Context
+{
+    public class TollFeeContext
+    {
+        private readonly ITollFeeStrategy _tollFeeStrategy;
+        private readonly bool _isETCPayment;
+
+        public TollFeeContext(ITollFeeStrategy tollFeeStrategy, bool isETCPayment)
+        {
+            _tollFeeStrategy = tollFeeStrategy;
+            _isETCPayment = isETCPayment;
+        }
+
+        public decimal CalculateTollFee(decimal distance)
+        {
+            decimal baseTollFee = _tollFeeStrategy.CalcukateTollFee(distance);
+            if (_isETCPayment)
+            {
+              baseTollFee = ApplyETCDiscount(baseTollFee);
+            }
+
+            return baseTollFee;
+        }
+
+        private decimal ApplyETCDiscount(decimal tollFee)
+        {
+            decimal discount = tollFee * 0.01m;
+            discount = discount > 20 ? 20 : discount;
+            return tollFee - discount;
+        }
+    }
+}
