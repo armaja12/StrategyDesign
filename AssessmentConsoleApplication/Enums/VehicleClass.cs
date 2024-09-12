d922ae90311c1a78e86b37c2f2753e1189f603de
armaja12 <janinelitada@gmail.com>
2024-09-12 15:06:47 +0800

    reupload

---
+++ AssessmentConsoleApplication/Enums/VehicleClass.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssessmentConsoleApplication.Enums
+{
+    public enum VehicleClass
+    {
+        Class1 = 1,
+        Class2,
+        Class3
+    }
+}
